fd2ba7bb91ed3b3858562234e34762f0e8c646df
leesy015599 <leesy015599@gmail.com>
2023-09-22 11:09:46 +0900

    Fin

--- Assets/Scripts/CharacterProfile.cs
+++ Assets/Scripts/CharacterProfile.cs
@@ -1,20 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+public class CharacterDefaultInformation
+{
+    public readonly string Name = "모험가";
+    public readonly int Level = 1;
+    public readonly int EXP = 0;
+    public readonly int MaxEXP = 10;
+    public readonly int Gold = 10000;
+}
 
 public class CharacterProfile : MonoBehaviour
 {
-    public readonly string Name = "모험가";
-
     [SerializeField] private TMP_Text _name;
-    [SerializeField] private TMP_Text EXP;
+    [SerializeField] private TMP_Text _ratioEXP;
+    [SerializeField] private Image _barEXP;
+    [SerializeField] private TMP_Text _level;
+    [SerializeField] private TMP_Text _gold;
     
     void Start()
     {
-        _name.text = Name;
+        CharacterDefaultInformation defaultValue = new();
+        _name.text = defaultValue.Name;
+        
+        StringBuilder str = new StringBuilder();
+        str.Append(defaultValue.EXP);
+        str.Append(" / ");
+        str.Append(defaultValue.MaxEXP);
+        _ratioEXP.text = str.ToString();
+        _barEXP.fillAmount = (float)(defaultValue.EXP) / defaultValue.MaxEXP;
+
+        _level.text = defaultValue.Level.ToString();
+        _gold.text = defaultValue.Gold.ToString();
     }
 }
